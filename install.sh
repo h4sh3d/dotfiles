95dc2f0138c013a219a48a613a8ae217f43359e2
Joel <joel@truelevel.io>
2019-08-05 09:31:46 +0200

    Update vim bundles

--- install.sh
+++ install.sh
@@ -80,30 +80,61 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 pushd ~/.vim/bundle
 if [ ! -d "goyo.vim" ]; then
     git clone https://github.com/junegunn/goyo.vim.git
+else
+    cd goyo.vim
+    git pull
+    cd ..
 fi
 if [ ! -d "nerdtree" ]; then
     git clone https://github.com/scrooloose/nerdtree.git
     #pushd nerdtree
     #git checkout tags/5.0.0
     #popd
+else
+    cd nerdtree
+    git pull
+    cd ..
 fi
 if [ ! -d "nord-vim" ]; then
     git clone https://github.com/arcticicestudio/nord-vim.git
     #pushd nord-vim
     #git checkout tags/v0.11.0
     #popd
+else
+    cd nord-vim
+    git pull
+    cd ..
 fi
 #if [ ! -d "typescript-vim" ]; then
 #    git clone https://github.com/leafgarland/typescript-vim.git
 #fi
 if [ ! -d "vim-airline" ]; then
     git clone https://github.com/bling/vim-airline
+else
+    cd vim-airline
+    git pull
+    cd ..
 fi
 if [ ! -d "vim-airline-themes" ]; then
     git clone https://github.com/vim-airline/vim-airline-themes
+else
+    cd vim-airline-themes
+    git pull
+    cd ..
 fi
 if [ ! -d "vim-gitgutter" ]; then
     git clone https://github.com/airblade/vim-gitgutter.git
+else
+    cd vim-gitgutter
+    git pull
+    cd ..
+fi
+if [ ! -d "vim-solidity" ]; then
+    git clone https://github.com/tomlion/vim-solidity.git
+else
+    cd vim-solidity
+    git pull
+    cd ..
 fi
 if [ ! -d "youcompleteme" ]; then
     git clone https://github.com/Valloric/youcompleteme.git
@@ -113,6 +144,10 @@ if [ ! -d "youcompleteme" ]; then
     popd
     # Install the Rust source code
     rustup component add rust-src
+else
+    cd youcompleteme
+    git pull
+    cd ..
 fi
 popd
 
